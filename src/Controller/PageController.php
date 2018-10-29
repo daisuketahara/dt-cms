93c653a4403f014b8ac709d0fe10c6a81836280b
Daisuke Tahara <d.tahara@intermention.com>
2018-10-29 04:19:30 +0100

    sitemap namespace

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -493,7 +493,7 @@ class PageController extends Controller
     public function generateSitemap(Request $request)
     {
         $xml = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
-        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">' . PHP_EOL;
 
         $defaultLocale = $this->getDoctrine()
             ->getRepository(Locale::class)
