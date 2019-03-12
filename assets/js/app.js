1dbb320bca8c09b39a5413efafa587af3a9901db
Daisuke Tahara <d.tahara@intermention.com>
2019-03-12 13:18:40 +0100

    Remove fontawesome from repo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,6 @@
 // ...
 
 require('../scss/app.scss');
-require('../scss/reviews.scss');
-require('../scss/step-blocks.scss');
-require('../scss/usability.scss');
-require('../scss/bootstrap-extended.scss');
-require('../scss/font-awesome-variables.scss');
 require('animate.css');
 
 require('./simple-cookie-bar.js');
@@ -16,5 +11,3 @@ require('./contact-form.js');
 // var $ = require('jquery');
 const $ = require('jquery');
 global.$ = global.jQuery = $;
-
-import '@fortawesome/fontawesome-free/js/all';
