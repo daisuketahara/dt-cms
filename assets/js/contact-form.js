ad6fd13f725af6f6bad6f03fc2ae852605920208
Daisuke Tahara <d.tahara@intermention.com>
2019-03-12 13:29:53 +0100

    Contact form fix if not exists

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,47 +1,50 @@
 jQuery(document).ready(function() {
 
-    var locale = jQuery('#contact-form').data('locale');
+    if(jQuery('#contact-form').length){
 
-	jQuery.ajax({
-		url: '/' + locale + '/ajax/contact/getform/',
-		type: 'POST',
-		data: {
-			locale: locale
-		},
-		dataType: 'html',
-		success: function(html) {
-            jQuery('#contact-form').html(html);
-        }
-    });
-
-    jQuery(document).on('submit', '#contact-form form', function(e) {
-
-        e.preventDefault();
-        jQuery('#contact-form [type=submit]').attr('disabled', 'disabled');
-
-        var phone = jQuery('#contact-phone').val();
-        var email = jQuery('#contact-email').val();
-        var message = jQuery('#contact-message').val();
+        var locale = jQuery('#contact-form').data('locale');
 
     	jQuery.ajax({
-    		url: '/' + locale + '/ajax/contact/post/',
+    		url: '/' + locale + '/ajax/contact/getform/',
     		type: 'POST',
     		data: {
-    			locale: locale,
-    			phone: phone,
-    			email: email,
-    			message: message,
+    			locale: locale
     		},
-    		dataType: 'json',
-    		success: function(json) {
-
-                if (json.result == 'valid') {
-                    jQuery('#contact-form').html(json.html);
-                } else {
-                    jQuery('#contact-form').prepend(json.html);
-                    jQuery('#contact-form [type=submit]').removeAttr("disabled");
-                }
+    		dataType: 'html',
+    		success: function(html) {
+                jQuery('#contact-form').html(html);
             }
         });
-    });
+
+        jQuery(document).on('submit', '#contact-form form', function(e) {
+
+            e.preventDefault();
+            jQuery('#contact-form [type=submit]').attr('disabled', 'disabled');
+
+            var phone = jQuery('#contact-phone').val();
+            var email = jQuery('#contact-email').val();
+            var message = jQuery('#contact-message').val();
+
+        	jQuery.ajax({
+        		url: '/' + locale + '/ajax/contact/post/',
+        		type: 'POST',
+        		data: {
+        			locale: locale,
+        			phone: phone,
+        			email: email,
+        			message: message,
+        		},
+        		dataType: 'json',
+        		success: function(json) {
+
+                    if (json.result == 'valid') {
+                        jQuery('#contact-form').html(json.html);
+                    } else {
+                        jQuery('#contact-form').prepend(json.html);
+                        jQuery('#contact-form [type=submit]').removeAttr("disabled");
+                    }
+                }
+            });
+        });
+    }
 });
