62b09cf05f337e1e952ddd7569ce627b5af4acb8
Daisuke Tahara <d.tahara@intermention.com>
2019-04-26 02:48:44 +0200

    Fix menu generator
    Signed-off-by: Daisuke Tahara <d.tahara@intermention.com>

--- src/Service/MenuService.php
+++ src/Service/MenuService.php
@@ -44,17 +44,18 @@ class MenuService
         $sql = "SELECT
         	mi.id AS id,
             mi.icon AS icon,
-        	CASE WHEN p.id IS NOT NULL THEN p.page_title ELSE mi.label END AS label,
+        	CASE WHEN p.id IS NOT NULL THEN pc.page_title ELSE mi.label END AS label,
         	CASE
-        		WHEN p.id IS NOT NULL AND l.default = 0 AND p.page_route = '' THEN CONCAT(l.locale,'/')
-        		WHEN p.id IS NOT NULL AND l.default = 0 THEN CONCAT(l.locale,'/',p.page_route,'/')
-        		WHEN p.id IS NOT NULL AND p.page_route = '' THEN ''
-        		WHEN p.id IS NOT NULL THEN CONCAT(p.page_route,'/')
+        		WHEN p.id IS NOT NULL AND l.default = 0 AND pc.page_route = '' THEN CONCAT(l.locale,'/')
+        		WHEN p.id IS NOT NULL AND l.default = 0 THEN CONCAT(l.locale,'/',pc.page_route,'/')
+        		WHEN p.id IS NOT NULL AND pc.page_route = '' THEN ''
+        		WHEN p.id IS NOT NULL THEN CONCAT(pc.page_route,'/')
         		ELSE mi.route
         	END AS route
         FROM menu_items AS mi
-        LEFT JOIN page AS p ON p.status = 1 AND p.locale_id = " . $locale->getId() . " AND (mi.page_id = p.id OR mi.page_id = p.default_id)
-        LEFT JOIN locale AS l ON l.id = p.locale_id
+        LEFT JOIN page AS p ON p.status = 1 AND mi.page_id = p.id
+        LEFT JOIN page_content AS pc ON p.id = pc.page_id AND pc.locale_id = " . $locale->getId() . "
+        LEFT JOIN locale AS l ON l.id = pc.locale_id
         WHERE mi.menu_id = " . $menuId . " AND mi.active = 1 AND mi.parent_id = 0
         ORDER BY mi.order ASC";
 
@@ -113,17 +114,18 @@ class MenuService
         $sql = "SELECT
         	mi.id AS id,
             mi.icon AS icon,
-        	CASE WHEN p.id IS NOT NULL THEN p.page_title ELSE mi.label END AS label,
+        	CASE WHEN p.id IS NOT NULL THEN pc.page_title ELSE mi.label END AS label,
         	CASE
-        		WHEN p.id IS NOT NULL AND l.default = 0 AND p.page_route = '' THEN CONCAT(l.locale,'/')
-        		WHEN p.id IS NOT NULL AND l.default = 0 THEN CONCAT(l.locale,'/',p.page_route,'/')
-        		WHEN p.id IS NOT NULL AND p.page_route = '' THEN ''
-        		WHEN p.id IS NOT NULL THEN CONCAT(p.page_route,'/')
+        		WHEN p.id IS NOT NULL AND l.default = 0 AND pc.page_route = '' THEN CONCAT(l.locale,'/')
+        		WHEN p.id IS NOT NULL AND l.default = 0 THEN CONCAT(l.locale,'/',pc.page_route,'/')
+        		WHEN p.id IS NOT NULL AND pc.page_route = '' THEN ''
+        		WHEN p.id IS NOT NULL THEN CONCAT(pc.page_route,'/')
         		ELSE mi.route
         	END AS route
         FROM menu_items AS mi
-        LEFT JOIN page AS p ON p.status = 1 AND p.locale_id = " . $locale->getId() . " AND (mi.page_id = p.id OR mi.page_id = p.default_id)
-        LEFT JOIN locale AS l ON l.id = p.locale_id
+        LEFT JOIN page AS p ON p.status = 1 AND mi.page_id = p.id
+        LEFT JOIN page_content AS pc ON p.id = pc.page_id AND pc.locale_id = " . $locale->getId() . "
+        LEFT JOIN locale AS l ON l.id = pc.locale_id
         WHERE mi.active = 1 AND mi.parent_id = " . $parentId . "
         ORDER BY mi.order ASC";
 
