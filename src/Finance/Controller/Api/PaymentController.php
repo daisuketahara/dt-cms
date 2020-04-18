08b4174b1de919aede6a1b5060c21d0fb8b274f8
Daisuke Tahara <hello@daisuketahara.com>
2020-04-18 13:45:23 +0200

    Fix webhook + check order payment

--- src/Finance/Controller/Api/PaymentController.php
+++ src/Finance/Controller/Api/PaymentController.php
@@ -66,6 +66,11 @@ class PaymentController extends AbstractController
 
         if ($paymentProvider == 'mollie') {
 
+            $key = $setting->get('finance.payment.provider.apikey');
+
+            $mollie = new \Mollie\Api\MollieApiClient();
+            $mollie->setApiKey($key);
+
             try {
                 /*
                  * Retrieve the payment's current state.
@@ -191,127 +196,23 @@ class PaymentController extends AbstractController
     /**
     * @Route("/api/v1/payment/verify/{id}/", name="api_payment_verify"), methods={"POST"})
     */
-    final public function verifyPayment(int $id, Request $request, SettingService $setting, MollieService $mollie, KernelInterface $kernel)
+    final public function verifyPayment($id, Request $request, SettingService $setting, MollieService $mollie, KernelInterface $kernel)
     {
-        $paymentProvider = $setting->get('finance.payment.provider');
+        $order = $this->getDoctrine()
+            ->getRepository(Orders::class)
+            ->find($id);
 
-        if ($paymentProvider == 'mollie') {
+        if ($order) {
 
-            try {
-                /*
-                 * Retrieve the payment's current state.
-                 */
-                $postId= $$id;
-                $payment = $mollie->payments->get($postId);
-                $orderId = $payment->metadata->order_id;
+            $response = [
+                'success' => true,
+                'message' => $order->getState()
+            ];
 
-                if (!empty($orderId)) {
-
-                    $response = array(
-                        'success' => false,
-                        'message' => 'Order id cannot be empty',
-                    );
-
-                    $json = $this->serializer->serialize($response, 'json');
-                    return $this->json($json);
-                }
-
-                $order = $this->getDoctrine()
-                    ->getRepository(Orders::class)
-                    ->find($orderId);
-
-                if (!$order) {
-
-                    $response = array(
-                        'success' => false,
-                        'message' => 'Cannot find order',
-                    );
-
-                    $json = $this->serializer->serialize($response, 'json');
-                    return $this->json($json);
-                }
-
-                // Get Subscription
-                $subscription = $order->getUserSubscription();
-
-                /*
-                 * Update the order in the database.
-                 */
-
-                if ($payment->isPaid() && !$payment->hasRefunds() && !$payment->hasChargebacks()) {
-                    /*
-                     * The payment is paid and isn't refunded or charged back.
-                     * At this point you'd probably want to start the process of delivering the product to the customer.
-                     */
-
-                     $order->setState('PAID');
-                     if ($subscription) $subscription->setActive(true);
-
-                } elseif ($payment->isOpen()) {
-                    /*
-                     * The payment is open.
-                     */
-                     $order->setState('OPEN');
-                     if ($subscription) $subscription->setActive(false);
-                } elseif ($payment->isPending()) {
-                    /*
-                     * The payment is pending.
-                     */
-                     $order->setState('PENDING');
-                     if ($subscription) $subscription->setActive(false);
-                } elseif ($payment->isFailed()) {
-                    /*
-                     * The payment has failed.
-                     */
-                } elseif ($payment->isExpired()) {
-                    /*
-                     * The payment is expired.
-                     */
-                     $order->setState('EXPIRED');
-                     if ($subscription) $subscription->setActive(false);
-                } elseif ($payment->isCanceled()) {
-                    /*
-                     * The payment has been canceled.
-                     */
-                     $order->setState('CANCEL');
-                     if ($subscription) $subscription->setActive(false);
-                } elseif ($payment->hasRefunds()) {
-                    /*
-                     * The payment has been (partially) refunded.
-                     * The status of the payment is still "paid"
-                     */
-                     $order->setState('REFUND');
-                     if ($subscription) $subscription->setActive(false);
-                } elseif ($payment->hasChargebacks()) {
-                    /*
-                     * The payment has been (partially) charged back.
-                     * The status of the payment is still "paid"
-                     */
-                     $order->setState('CHARGEBACK');
-                     if ($subscription) $subscription->setActive(false);
-                }
-
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($order);
-                $em->flush();
-                $em->persist($subscription);
-                $em->flush();
-
-                $response = [
-                    'success' => true,
-                    'message' => $order->getState(),
-                ];
-
-            } catch (\Mollie\Api\Exceptions\ApiException $e) {
-                $response = [
-                    'success' => false,
-                    'message' => "API call failed: " . htmlspecialchars($e->getMessage())
-                ];
-            }
         } else {
             $response = [
                 'success' => false,
-                'message' => 'Cannot process'
+                'message' => 'Cannot find order'
             ];
         }
 
